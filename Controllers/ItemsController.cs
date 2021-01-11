274deaed386b0651dd829838ad30b80548895d6f
hatrari <ham.atrari@gmail.com>
2021-01-11 10:37:33 +0100

    add CustomControllerFilter to the controller

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using FilterPipeline.Filters;
 
 namespace FilterPipeline.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [CustomControllerFilter]
     public class ItemsController : ControllerBase
     {
         [HttpGet]
