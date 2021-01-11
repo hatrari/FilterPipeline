5d26632e7708268848e6762522480dc90051f478
hatrari <ham.atrari@gmail.com>
2021-01-11 10:31:23 +0100

    init

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilterPipeline.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Items";
+        }
+    }
+}
